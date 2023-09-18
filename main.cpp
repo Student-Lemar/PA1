cdbaed3b17feb62e0894eb17d8fc1162168dda8e
Student-Lemar <82111383+Student-Lemar@users.noreply.github.com>
2023-09-17 21:45:01 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 // code source: https://www.codesdope.com/blog/article/c-linked-lists-in-c-singly-linked-list/
+//ADVANTAGES/DISADVANTAGES LINKED LIST: 
+//the advantages Dynamic Size: Linked lists can grow or shrink in size during runtime, making them very flexible. Elements can be easily added or removed without the need to allocate or deallocate memory in advance.
+//the disadvantage Random Access: Unlike arrays, linked lists don't support constant-time random access to elements. To access an element, you must traverse the list from the beginning, which can be slow for large lists.
+// code currently still unsuccessful
 
 #include <iostream>
 
@@ -44,20 +48,23 @@ public:
     }
     game()
     {
-        FILE command = fopen("command.csv", "r");
+        string command;
+        string dis;
+        FILE file = fopen("command.csv", "r");
+        
         
         
         int Displaychoice;
         vector<string> choices = {"1. Game Rules", "2. Play Game", "3. Load Previous Game", "4. Add Command", "5. Remove Command", "6. Exit"};
-        cout << "Please select a number option below:" << end1;
+        cout << "Please select a number option below:" << endl;
 
         for (int i = 0; i < choices.size(); i++){
-            cout << choices[i] << end1;
+            cout << choices[i] << endl;
         }
         cin >> (Displaychoice);
 
         if (Displaychoice == 1){
-            cout << "When the game starts it will display the description for the command and you the player\n will have to type the answer/command and if you get it correct you will recieve a point \nif you get it wrong you will lose a point" << end1;
+            cout << "When the game starts it will display the description for the command and you the player\n will have to type the answer/command and if you get it correct you will recieve a point \nif you get it wrong you will lose a point" << endl;
             return game();
         }
         else if(Displaychoice == 2){
