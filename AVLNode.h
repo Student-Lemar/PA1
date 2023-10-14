f6ad0b7a5feb2bcca72920fd98e3e65ca3e9a3d1
Lemar Encio <82111383+Student-Lemar@users.noreply.github.com>
2023-10-13 20:51:59 -0700

    Update AVLNode.h

--- AVLNode.h
+++ AVLNode.h
@@ -5,3 +5,21 @@
 #include <string>
 
 #include <vector>
+
+using namespace std;
+
+struct avlnode {
+  int node;
+  node *nextl, *nextr;
+  int height;
+};
+class Llist{
+  private:
+    node *head, *tail;
+}
+  public:
+    Llist(){
+      head = NULL;
+      tail = NULL;
+    }
+      
